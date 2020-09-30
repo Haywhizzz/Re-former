2108f407d06ec16042526fb50641deca89500fc3
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-09-30 16:04:14 +0200

    created the form(html way)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
-def create
-    
-end
-
-def new
-
+class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+      
+        if @user.save
+          redirect_to new_user_path
+        else
+            @user.errors.messages
+            fail
+        end
+      end
+    def new
+    end
+    def user_params 
+        params.require(:user).permit(:username,:email,:password)
+    end
 end
