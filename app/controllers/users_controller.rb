113866f1408d71af33254981927317a2af7fcf5c
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-09-30 21:48:00 +0200

    finished changes required by the tse

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
@@ -21,7 +20,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path
+      redirect_to new_user_path
     else
       render :edit
     end
